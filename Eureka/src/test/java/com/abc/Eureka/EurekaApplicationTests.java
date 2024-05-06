86975c62e9d1bb3d2c6ed889efbd8038054379b2
prashanthimuttha <163082857+prashanthimuttha@users.noreply.github.com>
2024-05-06 14:36:31 +0530

    Add files via upload

---
+++ Eureka/src/test/java/com/abc/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
